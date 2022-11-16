41b77636b0f9afb69cf21383c2f242167bb22c19
Thanadol Chomphoochan <tcpc@mit.edu>
2022-11-15 22:38:51 -0500

    vvpgit add .

--- test-bram-manager.sh
+++ test-bram-manager.sh
@@ -1,3 +1,3 @@
 cd src
-iverilog -g2012 ../src/bram_manager.sv ../src/xilinx_true_dual_port_read_first_1_clock_ram.v ../sim/bram_manager_tb.sv && ./a.out | tee ../bram_manager.txt
+iverilog -g2012 ../src/bram_manager.sv ../src/xilinx_true_dual_port_read_first_1_clock_ram.v ../sim/bram_manager_tb.sv && vvp a.out | tee ../bram_manager.txt
 rm -f a.out *.vcd
--- test-vector.sh
+++ test-vector.sh
@@ -1,3 +1,3 @@
 cd src
-iverilog -g2012 ../src/fixed_point_arith.sv ../src/vector_arith.sv ../sim/vector_arith_tb.sv && ./a.out | tee ../vector.txt
+iverilog -g2012 ../src/fixed_point_arith.sv ../src/vector_arith.sv ../sim/vector_arith_tb.sv && vvp a.out | tee ../vector.txt
 rm -f a.out *.vcd
