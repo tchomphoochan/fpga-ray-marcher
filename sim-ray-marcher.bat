c74d94217570484d404548af25e5cfee3d5a1e9f
Dillon DuPont <ddupont@mit.edu>
2022-11-19 16:09:18 -0500

    removed unused line

---
+++ sim-ray-marcher.bat
@@ -0,0 +1,5 @@
+cd src
+iverilog -g2012 ../src/ray_unit.sv ../src/ray_marcher.sv ../sim/full_ray_marcher_sim.sv
+vvp a.out | findstr "CMD" > ../full-ray-marcher-sim.txt
+cd ..
+echo "See full-ray-marcher-sim.txt"
