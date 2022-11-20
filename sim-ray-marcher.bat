0f4261ab3b81cb1900041b1770b5f43f005003a6
Thanadol Chomphoochan <tcpc@mit.edu>
2022-11-19 20:25:35 -0500

    fix again

--- sim-ray-marcher.bat
+++ sim-ray-marcher.bat
@@ -1,5 +1,6 @@
 cd src
-iverilog -g2012 ../src/ray_unit.sv ../src/ray_marcher.sv ../sim/full_ray_marcher_sim.sv
+iverilog -g2012 ../src/sdf_query.sv ../src/ray_unit.sv ../src/fp_inv_sqrt_folded.sv ../src/ray_generator.sv ../src/ray_generator_folded.sv ../src/ray_marcher.sv ../sim/full_ray_marcher_sim.sv
+vvp a.out > ../full-ray-marcher-sim.txt
 vvp a.out > ../full-ray-marcher-sim.txt
 cd ..
 echo "See full-ray-marcher-sim.txt"
