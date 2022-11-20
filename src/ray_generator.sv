94130b7a0a7e2a59d75e87add2a48ba1c432ca64
Thanadol Chomphoochan <tcpc@mit.edu>
2022-11-19 23:44:52 -0500

    make it consistent

--- src/ray_generator.sv
+++ src/ray_generator.sv
@@ -9,21 +9,27 @@ module ray_generator #(
   H_BITS = `H_BITS,
   V_BITS = `V_BITS
 ) (
-  input wire [H_BITS-1:0] hcount_in,
-  input wire [V_BITS-1:0] vcount_in,
-  input vec3 cam_pos,
-  input vec3 cam_forward,
-
+  input logic clk_in,
+  input logic rst_in,
+  input logic valid_in,
+  input logic [H_BITS-1:0] hcount_in,
+  input logic [V_BITS-1:0] vcount_in,
+  input vec3 cam_forward_in,
+  output logic valid_out,
+  output logic ready_out,
   output vec3 ray_direction_out
 );
+  assign ready_out = 1;
+  assign valid_out = valid_in;
+
 //   vec3 cam_ww = normalize(cam_target - cam_pos); // cam_forward
 
 // get normalized camera vectors
 //   vec3 cam_uu = normalize(cross(vec3(0,1,0), cam_ww)); // cam_right
 //   vec3 cam_vv = normalize(cross(cam_ww, cam_uu)); // cam_up
   vec3 cam_right, cam_up;
-  assign cam_right = vec3_cross(make_vec3(`FP_ZERO, `FP_ONE, `FP_ZERO), cam_forward);
-  assign cam_up = vec3_cross(cam_forward, cam_right);
+  assign cam_right = vec3_cross(make_vec3(`FP_ZERO, `FP_ONE, `FP_ZERO), cam_forward_in);
+  assign cam_up = vec3_cross(cam_forward_in, cam_right);
 // map y to about 0..1
 // 	 vec2 p = (2.0 * fragCoord - iResolution.xy) / iResolution.y;
   fp hcount_fp, vcount_fp;
@@ -41,7 +47,7 @@ module ray_generator #(
 
   vec3 rd0, rd1;
   assign rd0 = vec3_add(scaled_right, scaled_up);
-  assign rd1 = vec3_add(rd0, cam_forward);
+  assign rd1 = vec3_add(rd0, cam_forward_in);
   assign ray_direction_out = vec3_normed(rd1);
 
 endmodule // ray_generator
